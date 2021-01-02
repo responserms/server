b501d72392df4137d16e13f0525d4fc377cd392f
Scott Plunkett <scottplunkett15@gmail.com>
2021-01-02 02:48:30 +0000

    add license header

--- generate.go
+++ generate.go
@@ -1,5 +1,4 @@
 package response
 
 //go:generate go run ./ent/cmd/generate/main.go
-
 //go:generate go run github.com/99designs/gqlgen
--- internal/cluster/dmap.go
+++ internal/cluster/dmap.go
@@ -1,10 +1,16 @@
+// Copyright (c) 2021 Contaim, LLC
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 package cluster
 
 // DMapName represents the name of a distributed key/value map. This allows fetching a specific distributed map
 // without understanding the underlying string-based variation.
 type DMapName string
 
-type dmap struct {}
+type dmap struct{}
 
 func (dm *dmap) Get(key Key) ([]byte, error) {
 	return nil, nil
